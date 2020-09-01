570fb412e40f71d88e1a498f2f52d3c12b84dc7f
yuntao <yuntao@starmerx.com>
2020-09-01 18:49:25 +0800

    mysqldb

--- system/urls.py
+++ system/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-import django_cas_ng.views
 from django.views.generic import TemplateView
 
 
